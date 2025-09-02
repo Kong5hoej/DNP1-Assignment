344054abebe1d0adcc40ff347c14a447d6f76e87
Benjamin K. Hansen <bkh2604@gmail.com>
2025-09-02 15:29:45 +0200

    First part of the assignment

---
+++ Entities/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities;
+
+public class Post
+{
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public int Id { get; set; }
+    
+    public int UserId { get; set; }
+    
+}
